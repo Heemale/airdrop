d37434bd43324adc3e4798a8c918e0ad8d4d1028
Heemale <1916737051@qq.com>
2024-11-10 14:09:03 +0800

    feat(frontend): 设置自定义字体

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        orbitron: ["Orbitron"],
+        manrope: ["Manrope"],
+      },
     },
   },
   plugins: [],
