6dca3263976c923698805b5def936b825793b23f
Heemale <1916737051@qq.com>
2024-11-12 01:57:23 +0800

    feat(sdk): 增加合约异常码映射表

--- sdk/airdrop-sdk/src/airdrop/utils/constants.ts
+++ sdk/airdrop-sdk/src/airdrop/utils/constants.ts
@@ -1,1 +1,15 @@
 export const MODULE_CLOB = 'airdrop';
+
+export const ERROR_CODE: Record<bigint, string> = {
+    1n: "ERootUser",
+    2n: "EInvalidInviter",
+    3n: "EAlreadyBindInviter",
+    4n: "ENotBindInviter",
+    5n: "ECoinBalanceNotEnough",
+    6n: "EAlreadyBuyNode",
+    7n: "ENotBuyNode",
+    8n: "EInvalidAmount",
+    9n: "ERoundNotFound",
+    10n: "ERoundExited",
+    11n: "ENodeSoldOut"
+};
