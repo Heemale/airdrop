3cb04b8108ec68f11b1e2301ff67cc89fff337ab
Heemale <1916737051@qq.com>
2024-11-15 14:20:18 +0800

    feat(sdk): 增加常量

--- sdk/airdrop-sdk/src/utils/constants.ts
+++ sdk/airdrop-sdk/src/utils/constants.ts
@@ -1,16 +1,13 @@
-export const PACKAGE_ID =
-  '0x079cea6455be8b4609fe5271d7f33c4df132413f9ed0cc598e5099304b13884c';
+export const PACKAGE_ID = '';
 
-export const ADMIN_CAP =
-  '0xc28dc06da0f979cb290aabae852134fafeda25ca1a86be9a1537f742a2c46dd7';
+export const ADMIN_CAP = '';
 
-export const UPGRADE_CAP =
-  '0xbb6504ff7af9645d6e3e9bc437d7c48361e3b64b6fb1d40082faf4fbce77ecbe';
+export const UPGRADE_CAP = '';
 
-export const CONFIG =
-  '0x8f34d6eefc8a940ca9471448bdf060b2a05fa3a644d2451ae81cfabff70a71e9';
+export const INVITE = '';
 
-export const AIRDROPS =
-  '0xf51b1c7c43e436bdce3f4d2f8855c3384c3d2f7dfebaedbc382ca9edc1f6c5c4';
+export const NODE = '';
+
+export const AIRDROPS = '';
 
 export * from './constants';
