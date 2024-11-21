105c27b9d6f4c95595d1937852203915fa0ff883
Heemale <1916737051@qq.com>
2024-11-21 22:59:33 +0800

    feat(deploy): 更新脚本

--- apps/deploy/src/scripts/insertAirdrop.ts
+++ apps/deploy/src/scripts/insertAirdrop.ts
@@ -3,19 +3,14 @@ import { adminKeypair, airdropClient } from '@/sdk';
 import { ADMIN_CAP, AIRDROPS } from '@local/airdrop-sdk/utils';
 
 const insertAirdrop = async () => {
-  const admin = adminKeypair.getPublicKey().toSuiAddress();
-
-  // 定义空投参数
   const T = '0x2::sui::SUI'; // 替换为实际的代币类型，例如 SUI 或其他代币类型
   const airdrops = AIRDROPS; // 替换为实际的 Airdrop 对象 ID
-  const round = BigInt(2); // 空投轮次
-  const startTime = BigInt(Math.floor(Date.now() / 1000)); // 开始时间（当前时间戳）
-  const endTime = startTime + BigInt(360000); // 结束时间（1小时后）
+  const startTime = BigInt(Math.floor(Date.now())) + BigInt(86400 * 1000 * 7); // 开始时间（当前时间戳）
+  const endTime = startTime + BigInt(86400 * 1000 * 7); // 结束时间（1小时后）
   const totalShares = BigInt(100); // 空投份额
-  const totalBalance = BigInt(200000000); // 总金额
-  const description = 'Example Airdrop 2'; // 空投描述
-  const wallet = null; // 管理员钱包地址
-  const amount = BigInt(200000000);
+  const totalBalance = BigInt(100000000); // 总金额
+  const description = 'Example Airdrop 1'; // 空投描述
+  const amount = BigInt(100000000);
   const owner = adminKeypair.toSuiAddress(); // 使用管理员地址作为所有者
 
   // 调用 insert 方法创建添加空投的交易
@@ -23,7 +18,6 @@ const insertAirdrop = async () => {
     T,
     ADMIN_CAP,
     airdrops,
-    round,
     startTime,
     endTime,
     totalShares,
