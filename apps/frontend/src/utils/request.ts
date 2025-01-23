99cb30d4eac5e177f989e8ddbab0c181d734166a
Heemale <1916737051@qq.com>
2025-01-23 11:39:32 +0800

    feat(frontend): 增加请求接口示例

---
+++ apps/frontend/src/utils/request.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import {BASE_URL} from "@/config";
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 100000,
+});
+
+instance.interceptors.response.use(
+  (res) => {
+    if (res.status === 200 || res.status === 201) {
+      return res?.data;
+    }
+    return Promise.reject(res.data);
+  },
+  (err) => {
+    return Promise.reject(err);
+  },
+);
+
+export default instance;
