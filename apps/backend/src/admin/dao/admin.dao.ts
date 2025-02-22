8d762965b24023e6d1c3770c4c306998b543f09c
Heemale <1916737051@qq.com>
2025-02-22 10:23:18 +0800

    test(backend): 测试自动格式化

--- apps/backend/src/admin/dao/admin.dao.ts
+++ apps/backend/src/admin/dao/admin.dao.ts
@@ -2,7 +2,6 @@ import { prisma } from '@/config/prisma';
 import { Prisma } from '@prisma/client';
 
 export const create = async (data: Prisma.AdminCreateInput) => {
-	console.log('Test');
 	return prisma.admin.create({
 		data: {
 			...data,
