3258c9c98203d73a1e6e0006853392459a4f75ed
Heemale <1916737051@qq.com>
2025-02-25 16:40:33 +0800

    feat(admin): 后台对接显示图片

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3002',
+        pathname: '/images/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'suiworld.cyou',
+        port: '443',
+        pathname: '/images/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
