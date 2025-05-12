37e2f4ca3f382df343db68adf0d0acbb93a350c7
Heemale <1916737051@qq.com>
2025-05-12 11:50:28 +0800

    style(admin): 格式化代码

--- apps/admin/src/config/i18nProvider.ts
+++ apps/admin/src/config/i18nProvider.ts
@@ -1,8 +1,8 @@
 import polyglotI18nProvider from 'ra-i18n-polyglot';
 import chineseMessages from '@haxqer/ra-language-chinese';
 
-// @ts-ignore
 export const i18nProvider = polyglotI18nProvider(
+  // @ts-ignore
   () => chineseMessages,
   'zh_CN',
 );
