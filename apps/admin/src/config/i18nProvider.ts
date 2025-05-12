f2018e56605333b233a05b4282ee3935a24a7c9a
Heemale <1916737051@qq.com>
2025-05-12 12:20:01 +0800

    feat(admin): i18nProvider增加类型断言

--- apps/admin/src/config/i18nProvider.ts
+++ apps/admin/src/config/i18nProvider.ts
@@ -1,8 +1,12 @@
 import polyglotI18nProvider from 'ra-i18n-polyglot';
 import chineseMessages from '@haxqer/ra-language-chinese';
+import { TranslationMessages } from 'ra-core';
+
+const messages: Record<string, TranslationMessages> = {
+  zh_CN: chineseMessages as TranslationMessages,
+};
 
 export const i18nProvider = polyglotI18nProvider(
-  // @ts-ignore
-  () => chineseMessages,
+  (locale) => messages[locale],
   'zh_CN',
 );
